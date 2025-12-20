3442bf99b7c938b818a7a310d2f4cfaf03e024cd
dar-ryzhenko <daryna.com51@gmail.com>
2025-12-20 15:25:27 +0100

    add tasks and images to README.md

--- db.sql
+++ db.sql
@@ -146,3 +146,136 @@ INSERT INTO progress (enrollment_id, lesson_number, score, completed) VALUES
 (15, 1, 82, TRUE),
 (15, 2, 85, TRUE),
 (15, 3, 88, TRUE);
+
+
+-- Задача 1. Базові SELECT
+-- Вивести всіх студентів, які зареєструвалися після 
+SELECT *
+FROM students
+WHERE reg_date > '2024-01-01'
+ORDER BY reg_date;
+
+-- Вивести всі курси категорії "Data Science".
+SELECT *
+FROM courses
+WHERE category = 'Data Science';
+
+-------------------------------------------------------
+-- Задача 2. Групування та агрегація
+-- Порахувати кількість студентів у кожному місті.
+SELECT COUNT(*) AS student_count, city
+FROM students
+GROUP BY city;
+
+-- Порахувати кількість курсів у кожній категорії.
+SELECT COUNT(*) AS course_count, category    
+FROM courses
+GROUP BY category;
+
+-- Порахувати середню оцінку по кожному курсу.
+SELECT c.course_id, c.course_name, ROUND(AVG(p.score), 1) AS avg_score
+FROM courses c
+JOIN enrollments e ON c.course_id = e.course_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+GROUP BY c.course_id, c.course_name;
+
+
+-------------------------------------------------------
+-- Задача 3. JOIN‑аналіз
+-- Вивести список курсів разом з іменами викладачів.
+SELECT c.course_id, c.course_name, c.category, i.full_name AS instructor_name
+FROM courses c
+JOIN instructors i ON c.instructor_id = i.instructor_id
+ORDER BY c.course_name;
+
+-- Вивести студентів та назви курсів, на які вони записані.
+SELECT s.student_id, s.full_name AS student_name,
+    c.course_name
+FROM students s
+JOIN enrollments e ON s.student_id = e.student_id
+JOIN courses c ON c.course_id = e.course_id
+ORDER BY s.student_id, c.course_name
+
+-- Порахувати, скільки студентів у кожного викладача.
+SELECT i.instructor_id, i.full_name AS instuctor_name,
+    COUNT(e.student_id) AS student_count
+FROM instructors i 
+JOIN courses c ON c.instructor_id = i.instructor_id
+JOIN enrollments e ON e.course_id = c.course_id
+GROUP BY i.instructor_id,  i.full_name
+ORDER BY student_count
+
+
+-------------------------------------------------------
+-- Задача 4. Аналітика прогресу
+-- Порахувати середню оцінку кожного студента.
+SELECT s.student_id, s.full_name AS student_name,
+    ROUND(AVG(p.score), 1) AS avg_score
+FROM students s 
+JOIN enrollments e ON e.student_id = s.student_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+GROUP BY s.student_id, s.full_name
+ORDER BY avg_score;
+
+-- Порахувати відсоток завершених уроків для кожного курсу.
+SELECT c.course_id, c.course_name,
+    ROUND(100.0 * SUM(CASE 
+                    WHEN p.completed = TRUE 
+                    THEN 1 
+                    ELSE 0 
+                    END)/ COUNT(*), 2) AS completed_percent
+FROM courses c
+JOIN enrollments e ON c.course_id = e.course_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+GROUP BY c.course_id, c.course_name
+ORDER BY completed_percent;
+
+-- Знайти студентів, які завершили всі уроки у своїх курсах.
+SELECT s.student_id, s.full_name AS student_name
+FROM students s
+JOIN enrollments e ON s.student_id = e.student_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+GROUP BY s.student_id, s.full_name
+HAVING COUNT(*) = SUM(CASE
+                     WHEN p.completed = TRUE
+                     THEN 1 
+                     ELSE 0 
+                     END)
+ORDER BY s.student_id;
+
+-------------------------------------------------------
+-- Задача 5. Віконні функції
+-- Для кожного курсу визначити рейтинг студентів за середнім балом.
+SELECT c.course_id, c.course_name, s.student_id,
+s.full_name AS student_name,
+    ROUND(AVG(p.score), 2) AS avg_score,
+    RANK() OVER (PARTITION BY c.course_id
+    ORDER BY AVG(p.score) DESC) AS course_rank
+FROM courses c
+JOIN enrollments e ON c.course_id = e.course_id
+JOIN students s ON e.student_id = s.student_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+GROUP BY c.course_id, c.course_name, s.student_id, s.full_name
+ORDER BY c.course_name, course_rank;
+
+-- Порахувати кумулятивну кількість уроків, завершених студентом у хронологічному порядку.
+SELECT s.full_name, e.enroll_date,
+    SUM(CASE WHEN p.completed THEN 1 ELSE 0 END)
+        OVER (PARTITION BY s.student_id ORDER BY p.lesson_number) 
+        AS cumulative_completed
+FROM students s
+JOIN enrollments e ON s.student_id = e.student_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+ORDER BY s.student_id, e.enroll_date, p.lesson_number;
+
+-- Для кожної категорії курсів знайти топ‑1 курс за кількістю студентів.
+SELECT c.course_id, c.course_name, s.student_id, s.full_name AS student_name,
+    ROUND(AVG(p.score), 2) AS avg_score,
+    RANK() OVER (PARTITION BY c.course_id
+        ORDER BY AVG(p.score) DESC) AS course_rank
+FROM courses c
+JOIN enrollments e ON c.course_id = e.course_id
+JOIN students s ON e.student_id = s.student_id
+JOIN progress p ON e.enrollment_id = p.enrollment_id
+GROUP BY c.course_id, c.course_name, s.student_id, s.full_name
+ORDER BY c.course_name, course_rank;
